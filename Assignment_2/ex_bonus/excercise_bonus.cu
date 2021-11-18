f63c32ac4176a9ac1ad19c75d5e48652be2122d2
albinJoss <albjon@kth.se>
2021-11-18 15:40:08 +0100

    Made some small changes to clarify the code and make it more optimized.

--- Assignment_2/ex_bonus/excercise_bonus.cu
+++ Assignment_2/ex_bonus/excercise_bonus.cu
@@ -12,16 +12,16 @@
 #define NUM_BLOCKS 512
 
 __global__
-void calculateDouble(curandState *states,  int num_iterations, int *count)
+void calculateDouble(curandState *states, unsigned long long int num_iterations, long int *count)
 {
     double x, y, z;
     const int i = blockIdx.x * blockDim.x + threadIdx.x;
     curand_init(SEED, i, 0, &states[i]);
 
-    for(int j = 0; j < num_iterations; ++j)
+    for(unsigned long long int j = 0; j < num_iterations; ++j)
     {
-        x = curand_uniform(&states[i]);
-        y = curand_uniform(&states[i]);
+        x = curand_uniform_double(&states[i]);
+        y = curand_uniform_double(&states[i]);
 
         z = sqrt((x * x) + (y * y));
 
@@ -33,18 +33,18 @@ void calculateDouble(curandState *states,  int num_iterations, int *count)
 }
 
 __global__
-void calculateSingle(curandState *states,  int num_iterations, int *count)
+void calculateSingle(curandState *states, unsigned long long int num_iterations, long int *count)
 {
     float x, y, z;
     const int i = blockIdx.x * blockDim.x + threadIdx.x;
     curand_init(SEED, i, 0, &states[i]);
 
-    for(int j = 0; j < num_iterations; ++j)
+    for(unsigned long long int j = 0; j < num_iterations; ++j)
     {
         x = curand_uniform(&states[i]);
         y = curand_uniform(&states[i]);
 
-        z = sqrt((x*x) + (y * y));
+        z = sqrtf((x*x) + (y * y));
 
         if(z <=1.0)
         {
@@ -56,9 +56,9 @@ void calculateSingle(curandState *states,  int num_iterations, int *count)
 int main(int argc, char *argv[])
 {
     clock_t starting_time, end_time;
-    int num_blocks, single, TPB; 
-    int *d_count, *count;
-    unsigned long long num_iterations, iterations_per_thread;
+    int block_size, single, num_blocks; 
+    long int *d_count, *count;
+    unsigned long long int num_iterations, iterations_per_thread;
     double pi_double;
     float pi_single;
     unsigned long long sum = 0;
@@ -69,66 +69,66 @@ int main(int argc, char *argv[])
         case 1:
             single = 0;
             num_iterations = NUM_ITER;
-            num_blocks = NUM_BLOCKS;
+            block_size = NUM_BLOCKS;
             break;
 
         case 2:
             single = 0;
             num_iterations = NUM_ITER;
-            num_blocks = atoi(argv[1]);
+            block_size = atoi(argv[1]);
             break;
         
         case 3:
             single = 0;
-            num_iterations = atoi(argv[2]);
-            num_blocks = atoi(argv[1]);
+            num_iterations = atoll(argv[2]);
+            block_size = atoi(argv[1]);
             break;
 
         default:
             single = atoi(argv[3]);
-            num_iterations = atoi(argv[2]);
-            num_blocks = atoi(argv[1]);
+            num_iterations = atoll(argv[2]);
+            block_size = atoi(argv[1]);
             break;
     }
 
-    TPB =  131072 /  num_blocks;
-    iterations_per_thread = num_iterations / (num_blocks * TPB); 
-    printf("%llu\n", iterations_per_thread);
-    cudaMalloc((void**)&dev_random, TPB * num_blocks * sizeof(curandState));
-    cudaMalloc(&d_count, num_blocks * TPB * sizeof(int));
-    count = (int*)malloc(num_blocks * TPB * sizeof(int));
-    //cudaMemcpy(d_count, count, num_blocks * TPB, cudaMemcpyHostToDevice);
+    num_blocks =  10496/ block_size;
+    iterations_per_thread = num_iterations / (block_size * num_blocks); 
+    
+    cudaMalloc((void**)&dev_random, num_blocks * block_size * sizeof(curandState));
+    cudaMalloc(&d_count, block_size * num_blocks * sizeof(long int));
+    count = (long int*)malloc(block_size * num_blocks * sizeof(long int));
+    
 
     
     if(single == 0)
     {
         starting_time = clock();
-        calculateDouble<<<num_blocks, TPB>>>(dev_random, iterations_per_thread, d_count);
+        calculateDouble<<<num_blocks, block_size>>>(dev_random, iterations_per_thread, d_count);
         cudaDeviceSynchronize();
         end_time = clock();
-        cudaMemcpy(count, d_count, num_blocks * TPB * sizeof(int), cudaMemcpyDeviceToHost);
-        for(int i = 0; i < num_blocks * TPB; ++i)
+        cudaMemcpy(count, d_count, block_size * num_blocks * sizeof(int), cudaMemcpyDeviceToHost);
+        for(int i = 0; i < block_size * num_blocks; ++i)
         {
             
             sum += count[i];
         }
-        printf("%llu\n", sum);
+        
         pi_double = ((double)sum / ((double) num_iterations)) * 4.0;
-        printf("Done with the simulation on the for double precision, using %ld iterations and a block size of %d. It took %lf seconds on the GPU and a total of %lf seconds to do and pi was estimated to %.15f\n", (long int)num_iterations, num_blocks, (double) (end_time - starting_time) / CLOCKS_PER_SEC, (double) (clock() - starting_time) / CLOCKS_PER_SEC, pi_double);
+        printf("Done with the simulation on the for double precision, using %llu iterations and a block size of %d. It took %lf seconds on the GPU and a total of %lf seconds to do and pi was estimated to %.15f\n", num_iterations, block_size, (double) (end_time - starting_time) / CLOCKS_PER_SEC, (double) (clock() - starting_time) / CLOCKS_PER_SEC, pi_double);
     }
     else
     {
         starting_time = clock();
-        calculateSingle<<<num_blocks, TPB>>>(dev_random, iterations_per_thread, d_count);;
+        calculateSingle<<<num_blocks, block_size>>>(dev_random, iterations_per_thread, d_count);;
         cudaDeviceSynchronize();
         end_time = clock();
-        cudaMemcpy(count, d_count, num_blocks * TPB * sizeof(int), cudaMemcpyDeviceToHost);
-        for(int i = 0; i < num_blocks * TPB; ++i)
+        cudaMemcpy(count, d_count, block_size * num_blocks * sizeof(int), cudaMemcpyDeviceToHost);
+        for(int i = 0; i < block_size * num_blocks; ++i)
         {
             sum += count[i];
         }
-        pi_single = ((float)sum /((float) iterations_per_thread *  TPB * num_blocks)) * 4.0;
-        printf("Done with the simulation on the for single precision, using %ld iterations and a block size of %d. It took %lf seconds on the GPU and a total of %lf seconds to do and pi was estimated to %.15f\n", (long int)num_iterations, num_blocks, (double) (end_time - starting_time) / CLOCKS_PER_SEC, (double) (clock() - starting_time) / CLOCKS_PER_SEC, pi_single);
+        pi_single = ((float)sum /((float) iterations_per_thread *  num_blocks * block_size)) * 4.0;
+        printf("Done with the simulation on the for single precision, using %llu iterations and a block size of %d. It took %lf seconds on the GPU and a total of %lf seconds to do and pi was estimated to %.15f\n", num_iterations, block_size, (double) (end_time - starting_time) / CLOCKS_PER_SEC, (double) (clock() - starting_time) / CLOCKS_PER_SEC, pi_single);
     }
     
     cudaFree(dev_random);
